c5c54baa5de9984bbfc2caa49fd6220becf0601e
Manoj Waikar <mmwaikar@gmail.com>
2025-08-25 16:24:20 +0200

    Basic configuration.
    - add logos, favicon
    - add docker related files
    - update dependency versions
    - update prettier, page.svelte etc.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    // Using hot module replacement (HMR) with custom elements (aka web
+    // components) does not work because a custom element cannot be updated once
+    // registered, see https://github.com/WICG/webcomponents/issues/820.
+    // Therefore we do a full page reload instead of HMR.
+    if (import.meta.hot) {
+      import.meta.hot.on("vite:beforeUpdate", () => {
+        window.location.reload();
+      });
+    }
+  
+    // Import Lens CSS and JS bundles
+    import "@samply/lens/style.css";
+    import "@samply/lens";
+  
+    import App from "../App.svelte";
+  </script>
+  
+  <App />
+  
